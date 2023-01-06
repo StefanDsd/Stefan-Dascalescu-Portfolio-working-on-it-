03a4d98301d4b501884c97850331459bd4672fe1
StefanDsd <stefan_dascalescu92@gmail.com>
2023-01-06 14:23:05 +0100

    feat(navbar): :art: navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-
-import './App.css';
-import Home
- from './Components/Home';
+import "./App.css";
+import Home from "./Components/Home";
+import Header from "./Components/Header";
+import Nav from "./Components/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
+      <Nav />
     </div>
   );
 }
